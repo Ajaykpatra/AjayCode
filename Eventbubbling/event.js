508aff472317a6b44b619a7b351cf661a1185f78
avay <patraavaykumar@gmail.com>
2024-02-27 22:47:17 +0530

    EventBubbling code

---
+++ Eventbubbling/event.js
@@ -0,0 +1,11 @@
+//Event Bubbling & capturing
+//stop propagation,immediate propagation & prevent Default
+
+
+// var div=document.querySelector("div");
+
+// div.addEventListener("click",()=>{
+//     console.log("div")
+// })
+let sum = 2 + 2.15;
+console.log(sum);
