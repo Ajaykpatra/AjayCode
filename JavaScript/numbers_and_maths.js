c074b187e394022d49fc907cac89eeb5f8d72ccd
avay <patraavaykumar@gmail.com>
2024-04-27 15:49:04 +0530

    add some javascript

--- JavaScript/numbers_and_maths.js
+++ JavaScript/numbers_and_maths.js
@@ -1,7 +1,33 @@
-const score = 400
+/*const score = 400
 console.log(score);
 
 const balance = new Number(200)
 console.log(balance);
 
-console.log(balance.toString());
+console.log(balance.toString());*/
+
+//+++++++++++++++++MATHS+++++++++++
+
+
+//console.log(Math);
+
+// console.log(Math.abs(-5));
+// console.log(Math.round(5.7));
+// console.log(Math.ceil(2.7));
+// console.log(Math.floor(5.7));
+
+console.log(Math.random());
+console.log(Math.random()*10);
+console.log((Math.random()*10)+1);
+
+
+
+
+
+
+
+
+
+
+
+
