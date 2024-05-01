b2825ae15222f8d07211b7c61f5f0bcbd028cc54
avay <patraavaykumar@gmail.com>
2024-05-01 21:42:32 +0530

    Adding some js

---
+++ JavaScript/function.js
@@ -0,0 +1,52 @@
+// function sayMyName(){
+
+//     console.log("A");
+//     console.log("J");
+//     console.log("A");
+//     console.log("y");
+
+// }
+//sayMyName()
+
+
+// function addTwoNumbers(number1,number2){
+//     // console.log(number1+number2);
+
+
+//     //OR
+
+//     // let result = number1+number2
+//     // return result
+
+//    //OR
+
+//    return number1+number2
+
+// }
+
+// addTwoNumbers(3,6)
+
+// const result= addTwoNumbers(3,6)
+
+// console.log("Result :",result);
+
+function loginUserMessage(username="Sam"){
+
+    if(!username){
+        console.log("please enter a user name");
+        return
+    }
+    return`${username} just logged in`      
+}
+
+ //console.log(loginUserMessage("Ajay"))
+
+//console.log(loginUserMessage())  //returns Undefined
+
+
+
+
+
+
+
+
--- JavaScript/numbers_and_maths.js
+++ JavaScript/numbers_and_maths.js
@@ -19,6 +19,14 @@ console.log(balance.toString());*/
 console.log(Math.random());
 console.log(Math.random()*10);
 console.log((Math.random()*10)+1);
+console.log(Math.floor(Math.random()*10)+1);
+
+
+const min = 10
+const max = 20
+console.log(Math.floor(Math.random()*(max-min)+1)+min);
+console.log(Math.floor(Math.random()*(max-min)+1)+max);
+
 
 
 
