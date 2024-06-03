b87d95b28866770873b2706796aaac6cfc6d00ab
avay <patraavaykumar@gmail.com>
2024-06-03 11:07:27 +0530

    adding some javascript

---
+++ JavaScript/scopes.js
@@ -0,0 +1,17 @@
+let a=300
+var c = 654
+if(true){
+    let a=34
+    const b=54
+    console.log("INNER :",a);
+}
+
+for (let i = 0; i < array.length; i++) {
+    const element = array[i];
+    
+}   
+
+
+console.log(a);
+//console.log(b);
+console.log(c);
